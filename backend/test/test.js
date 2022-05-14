43b00321edd7ab885855f3b1d96faf718339af83
JisanFahim <fahimjason@users.noreply.github.com>
2022-05-14 23:36:17 +0600

    Add test

--- backend/test/test.js
+++ backend/test/test.js
@@ -7,75 +7,58 @@ const sinonChai = require('sinon-chai');
 chai.use(sinonChai);
 const rewire = require('rewire');
 
-var sequelize = require('');
+var { Sequelize } = require('sequelize');
 
-var users = rewire('./users');
-var User = require('./models/user');
+var designations = rewire('../controller/designation.controller');
+var Designation = require('../models/designation.model');
 
-var sandbox = sinon.sandbox.create();
+var sandbox = sinon.sandbox.createSandbox();
 
-describe('users', ()=>{
+describe('designations', () => {
     let findStub;
-    let deleteStub;
-    let sampleUser;
-    let mailerStub;
+    let sampleDesignation; Tt
 
-    beforeEach(()=>{
+    beforeEach(() => {
         sampleDesignation = {
             id: 123,
-            title: 'foo',
-            email: 'foo@bar.com',
+            title: 'CTO',
+            parent_id: 0,
+            description: null,
             save: sandbox.stub().resolves()
         }
 
-        findStub = sandbox.stub(mongoose.Model, 'findById').resolves(sampleUser);
-        deleteStub = sandbox.stub(mongoose.Model, 'remove').resolves('fake_remove_result');
+        findStub = sandbox.stub(Designation, 'findOne').resolves(sampleDesignation);
     })
 
-    afterEach(()=>{
+    afterEach(() => {
         sandbox.restore();
-        users = rewire('./users');
+        users = rewire('../controller/designation.controller.js');
     })
 
-    context('get', ()=>{
-        it('should check for an id', (done)=>{
-            users.get(null, (err, result)=>{
+    context('get', () => {
+        it('should check for an id', (done) => {
+            users.get(null, (err, result) => {
                 expect(err).to.exist;
                 expect(err.message).to.equal('Invalid user id');
                 done();
             })
         })
 
-        it('should call findDesignationById with id and return result', (done)=>{
+        it('should call findDesignationById with id and return result', (done) => {
             sandbox.restore();
-            let stub = sandbox.stub(mongoose.Model, 'findById').yields(null, {name: 'foo'});
+            let stub = sandbox.stub(Designation, 'findOne').yields(null, { title: 'foo' });
 
-            users.get(123, (err, result)=>{
+            users.get(123, (err, result) => {
                 expect(err).to.not.exist;
                 expect(stub).to.have.been.calledOnce;
                 expect(stub).to.have.been.calledWith(123);
                 expect(result).to.be.a('object');
-                expect(result).to.have.property('name').to.equal('foo');
+                expect(result).to.have.property('title').to.equal('foo');
+                expect(result).to.have.property('parent_id').to.equal(0);
 
                 done();
             })
-        })
-
-        it('should catch error if there is one', (done)=>{
-            sandbox.restore();
-            let stub = sandbox.stub(mongoose.Model,'findById').yields(new Error('fake'));
-
-            users.get(123, (err, result)=>{
-                expect(result).to.not.exist;
-                expect(err).to.exist;
-                expect(err).to.be.instanceOf(Error);
-                expect(stub).to.have.been.calledWith(123);
-                expect(err.message).to.equal('fake');
-
-                done();
-            })
-
-        })
-    })
+        });
+    });
 
 })
